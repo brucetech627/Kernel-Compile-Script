e183aac4c819059f8f87010a3cef6dcf35c9cf32
brucetech627 <66194009+brucetech627@users.noreply.github.com>
2021-06-01 00:49:56 +0530

    Update clone.sh

--- clone.sh
+++ clone.sh
@@ -1,6 +1,6 @@
 # Kernel Clonning Script
 
-git clone https://github.com/brucetech627/kernel_xiaomi_sweet storm
+git clone --depth=1 https://github.com/brucetech627/kernel_xiaomi_sweet storm
 cd storm
 git clone --depth=1 -b master https://github.com/MASTERGUY/proton-clang clang
 git clone --depth=1 https://github.com/stormbreaker-project/AnyKernel3 -b sweet AnyKernel
